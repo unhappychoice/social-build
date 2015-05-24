5a2fd4772f4cdc318e85d4fda789adf992f02962
unhappychoice <unhappychoice@gmail.com>
2015-05-24 21:40:55 +0900

    remove unnecessary comment

--- src/main/kotlin/jp/dip/socialbuild/extension/AnyExtension.kt
+++ src/main/kotlin/jp/dip/socialbuild/extension/AnyExtension.kt
@@ -6,7 +6,4 @@ import java.sql.Date
  * Created by unhappychoice on 2015/05/24.
  */
 
-// -----------------------------------------------------------------------------------------
-// private
-
 public fun Any.currentDate(): Date = Date(java.util.Date().getTime())
